209938ae36a7c96ff050b07425529bd21d40de81
Mojsovski <amrattategar30@gmail.com>
2025-01-22 11:58:32 +0700

    add : auth login

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import * as Yup from "yup";
 import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
 
 type TRegister = {
   fullName: string;
@@ -8,6 +9,11 @@ type TRegister = {
   email: string;
   password: string;
   confirmPassword: string;
+};
+
+type TLogin = {
+  identifier: string;
+  password: string;
 };
 
 const registerValidateSchema = Yup.object({
@@ -44,6 +50,54 @@ export default {
       res.status(200).json({
         message: "Registrasi Berhasil",
         data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async login(req: Request, res: Response) {
+    const { identifier, password } = req.body as unknown as TLogin;
+    try {
+      //ambil data user berdasarkan "identifier" => email dan username
+      const userbyIdentifier = await UserModel.findOne({
+        $or: [
+          {
+            email: identifier,
+          },
+          {
+            userName: identifier,
+          },
+        ],
+      });
+
+      if (!userbyIdentifier) {
+        res.status(403).json({
+          message: "user not found",
+          data: null,
+        });
+        return;
+      }
+
+      //validasi password
+      const validatePassword: boolean =
+        encrypt(password) === userbyIdentifier.password;
+
+      if (!validatePassword) {
+        res.status(403).json({
+          message: "password salah",
+          data: null,
+        });
+        return;
+      }
+
+      res.status(200).json({
+        message: "berhasil login",
+        data: userbyIdentifier,
       });
     } catch (error) {
       const err = error as unknown as Error;
