671383ebc8fb75f7d5f3fd38b127ba1bcf3a0ac8
Mojsovski <amrattategar30@gmail.com>
2025-05-13 17:20:37 +0700

    add: error handling be

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import db from "./utils/database";
 import docs from "./docs/route";
 import cors from "cors";
+import errorMiddleware from "./middleware/error.middleware";
 
 async function init() {
   try {
@@ -28,6 +29,10 @@ async function init() {
     app.use("/api", router);
 
     docs(app);
+
+    // error handling
+    app.use(errorMiddleware.serverRoute());
+    app.use(errorMiddleware.serverError());
 
     app.listen(PORT, () => {
       console.log(`Server Berjalan di http://localhost:${PORT}`);
